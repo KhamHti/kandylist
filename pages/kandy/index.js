f78b85f944290f1e195ebc8ecac9126737d6d5f6
KhamHti <90329989+KhamHti@users.noreply.github.com>
2023-03-25 23:52:19 +0630

    fetching data
    getStaticProps and styles for fetched data

--- pages/kandy/index.js
+++ pages/kandy/index.js
@@ -1,5 +1,15 @@
 import Head from "next/head";
-const Kandy = () => {
+import styles from "../../styles/Kandy.module.css";
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  return {
+    props: { kandies: data },
+  };
+};
+
+const Kandy = ({ kandies }) => {
   return (
     <>
       <Head>
@@ -7,8 +17,17 @@ const Kandy = () => {
         <meta name="keywords" content="luffy" />
       </Head>
       <div>
-        <h1>Kandy</h1>
-        <p>lorem blah blah blah</p>
+        <h1>Kandys</h1>
+        {kandies.map((kandy) => (
+          <div key={kandy.id}>
+            <a className={styles.single}>
+              <h3>{kandy.name}</h3>
+              <h5>username: {kandy.username}</h5>
+              <h5>email: {kandy.email}</h5>
+              <h5>website: {kandy.website}</h5>
+            </a>
+          </div>
+        ))}
       </div>
     </>
   );
