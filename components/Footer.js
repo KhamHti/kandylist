f873cff744b0eface4eed9864f283100deeeee2b
KhamHti <90329989+KhamHti@users.noreply.github.com>
2023-03-25 13:04:12 +0630

    components
    adding components

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div>
+      <p>@Copyright 2023 @kandymolly</p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ components/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <h1>Kandy Lists</h1>
+      </div>
+      <a>Home</a>
+      <a>About</a>
+      <a>Kandy Listing</a>
+    </nav>
+  );
+};
+
+export default Navbar;
