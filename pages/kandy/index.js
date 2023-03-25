60fdb91c3aa0e71944648995d7a508cb82ffa5ab
KhamHti <90329989+KhamHti@users.noreply.github.com>
2023-03-25 13:04:51 +0630

    kandy
    adding kandy routes

---
+++ pages/kandy/index.js
@@ -0,0 +1,10 @@
+const Kandy = () => {
+  return (
+    <div>
+      <h1>Kandy molly talkie</h1>
+      <p>lorem blah blah blah</p>
+    </div>
+  );
+};
+
+export default Kandy;
---
+++ pages/kandy/test.js
@@ -0,0 +1,11 @@
+const Test = () => {
+  return (
+    <div>
+      <h1>Test</h1>
+      <p>hello world.</p>
+      <p>This is kandy subfolder and this is test route.</p>
+    </div>
+  );
+};
+
+export default Test;
